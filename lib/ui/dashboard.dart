62792bf6c3c02ecfbaa7d2b25d162f65659b4872
aurelioo29 <aureliolucio5@gmail.com>
2024-07-04 21:23:53 +0700

    renew some code during unused

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pos_resto/core/session_manager.dart';
 import 'package:flutter_pos_resto/cubit/appsetting_cubit.dart';
+import 'package:flutter_pos_resto/ui/table_resto_index_page.dart';
 // import '../bloc/table_resto/index/table_resto_index_bloc.dart';
 
 class HomePage extends StatelessWidget {
-
   final sessionManager = SessionManager();
 
   @override
@@ -26,55 +26,40 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Text('Token : ${sessionManager.getActivateToken()}'),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           Text('ID : ${sessionManager.getActivateId()}'),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           Text('FirstName : ${sessionManager.getActivateFirstName()}'),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           Text('LastName : ${sessionManager.getActivateLastName()}'),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           Text('Email : ${sessionManager.getActivateEmail()}'),
-          SizedBox(height: 5,),
-          // GridView.count(
-          //   crossAxisCount: 2, // Dua kolom dalam grid
-          //   crossAxisSpacing: 10,
-          //   mainAxisSpacing: 10,
-          //   children: [
-          //     DashboardButton(
-          //       icon: Icons.shopping_cart,
-          //       label: 'Keranjang',
-          //       onPressed: () {
-          //         // Aksi ketika tombol keranjang ditekan
-          //       },
-          //     ),
-          //     DashboardButton(
-          //       icon: Icons.restaurant_menu,
-          //       label: 'Menu Resto',
-          //       onPressed: () {
-          //         // Aksi ketika tombol menu resto ditekan
-          //       },
-          //     ),
-          //     DashboardButton(
-          //       icon: Icons.table_chart,
-          //       label: 'Table Resto',
-          //       onPressed: () {
-          //         // Aksi ketika tombol table resto ditekan
-          //         Navigator.push(
-          //           context,
-          //           MaterialPageRoute(
-          //             builder: (context) => BlocProvider(
-          //               create: (context) => TableRestoIndexBloc()..add(GetTableRestoIndexEvent()),
-          //               child: TableRestoIndexPage(),
-          //             ),
-          //           ),
-          //         );
-          //       },
-          //     ),
-          //   ],
-          // ),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
+          FilledButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TableRestoIndexPage(),
+                  ),
+                );
+              },
+              child: Text('Pesanan Makanan')),
+          SizedBox(
+            height: 5,
+          ),
           ElevatedButton(
-              onPressed: (){
+              onPressed: () {
                 sessionManager.signOut();
                 context.read<AppsettingCubit>().checkSession();
               },
@@ -90,7 +75,8 @@ class DashboardButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
 
-  const DashboardButton({required this.icon, required this.label, required this.onPressed});
+  const DashboardButton(
+      {required this.icon, required this.label, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
